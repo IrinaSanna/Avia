0930de15252f227c5101315615d8c6261b65ef2f
Irina D <RabotaYd2021@yandex.ru>
2024-01-30 18:26:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int tiketPrace = 15500;
+        int mileAmount = 20;
+        int bonus = tiketPrace / mileAmount;
+
+        System.out.println("Итого бонусных миль: " + bonus);
+    }
+}
